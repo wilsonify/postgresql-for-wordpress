f83e85e5f2824b9142ca4309997ff3204dbaac5f
wilsonify <tom.andrew.wilson@gmail.com>
2026-07-01 07:59:24 -0400

    style: rename RewriteTest class for proper casing and improve variable naming in tests

--- tests/rewriteTest.php
+++ tests/rewriteTest.php
@@ -11,24 +11,21 @@ if (!defined('WPINC')) {
 
 require_once __DIR__ . "/../pg4wp/db.php";
 
-final class rewriteTest extends TestCase
+final class RewriteTest extends TestCase
 {
     public function test_it_can_rewrite_users_admin_query()
     {
-
         $sql = 'SELECT COUNT(NULLIF(`meta_value` LIKE \'%"administrator"%\', false)), COUNT(NULLIF(`meta_value` = \'a:0:{}\', false)), COUNT(*) FROM wp_usermeta INNER JOIN wp_users ON user_id = ID WHERE meta_key = \'wp_capabilities\'';
         $expected = 'SELECT COUNT(NULLIF(meta_value ILIKE \'%"administrator"%\', false)) AS count0, COUNT(NULLIF(meta_value = \'a:0:{}\', false)) AS count1, COUNT(*) FROM wp_usermeta INNER JOIN wp_users ON user_id = "ID" WHERE meta_key = \'wp_capabilities\'';
-        $postgresql = pg4wp_rewrite($sql);
-        $this->assertSame($postgresql, $expected);
+        $actual = pg4wp_rewrite($sql);
+        $this->assertSame($expected, $actual);
     }
-
 
     public function test_it_adds_group_by()
     {
-
         $sql = 'SELECT COUNT(id), username FROM users';
         $expected = 'SELECT COUNT(id) AS count0, username FROM users GROUP BY username';
-        $postgresql = pg4wp_rewrite($sql);
-        $this->assertSame($postgresql, $expected);
+        $actual = pg4wp_rewrite($sql);
+        $this->assertSame($expected, $actual);
     }
 }
